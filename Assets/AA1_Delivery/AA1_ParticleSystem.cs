e48e4bb107a894066d3d5fbea923facf8f00da41
PAUGIROGARCIA <pau.giro@enti.cat>
2024-02-28 20:06:02 +0100

    Cascade created

--- Assets/AA1_Delivery/AA1_ParticleSystem.cs
+++ Assets/AA1_Delivery/AA1_ParticleSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using static AA1_ParticleSystem;
 
 [System.Serializable]
 public class AA1_ParticleSystem
@@ -6,8 +7,10 @@ public class AA1_ParticleSystem
     [System.Serializable]
     public struct Settings
     {
+        public float pool;
         public Vector3C gravity;
         public float bounce;
+        public bool spawnsFromCascade;
     }
     public Settings settings;
 
@@ -16,7 +19,14 @@ public class AA1_ParticleSystem
     {
         public Vector3C PointA;
         public Vector3C PointB;
-        public float particlesPerSecond;
+        public Vector3C direction;
+        public bool aleatoryDirection;
+        public float minForce;
+        public float maxForce;
+        public float minParticlesPerSecond;
+        public float maxParticlesPerSecond;
+        public float minParticleLife;
+        public float maxParticleLife;
     }
     public SettingsCascade settingsCascade;
 
@@ -26,7 +36,12 @@ public class AA1_ParticleSystem
         public Vector3C Start;
         public Vector3C Direction;
         public float angle;
-        public float particlesPerSecond;
+        public float minForce;
+        public float maxForce;
+        public float minParticlesPerSecond;
+        public float maxParticlesPerSecond;
+        public float minParticleLife;
+        public float maxParticleLife;
     }
     public SettingsCannon settingsCannon;
 
@@ -43,21 +58,96 @@ public class AA1_ParticleSystem
     {
         public Vector3C position;
         public Vector3C positionLast;
+        public Vector3C velocity;
         public Vector3C acceleration;
+        public Vector3C force;
+        public float particleLife;
         public float size;
+        public float mass;
+        public bool active;
+        public bool isInitialized;
+
+        public Particle(bool isActive)
+        {
+            position = Vector3C.zero;
+            positionLast = Vector3C.zero;
+            velocity = Vector3C.zero;
+            acceleration = Vector3C.zero;
+            force = Vector3C.zero;
+            particleLife = 10.0f;
+            size = 0.03f;
+            mass = 1.0f;
+            active = isActive;
+            isInitialized = true;
+        }
+
+        public void AddForce(Vector3C force)
+        {
+            this.force += force;
+        }
     }
 
     Random rnd = new Random();
+    Particle[] particles = new Particle[1000];
 
     public Particle[] Update(float dt)
     {
-        Particle[] particles = new Particle[100];
+        if (settings.spawnsFromCascade)
+        {
+            InitCascade();
+        }
+
+        SolverEuler(dt);
+        
+
+        return particles;
+    }
+
+    public void SolverEuler(float dt)
+    {
         for (int i = 0; i < particles.Length; ++i)
         {
-            particles[i].position = new Vector3C((float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f, (float)rnd.NextDouble() - 0.5f);
-            particles[i].size = 0.03f;
+            if(particles[i].active)
+            {
+                particles[i].force += settings.gravity;
+                particles[i].acceleration = particles[i].force / particles[i].mass;
+                particles[i].velocity += particles[i].acceleration * dt;
+                particles[i].position += particles[i].velocity * dt;
+
+                particles[i].positionLast = particles[i].position;
+                particles[i].force = Vector3C.zero;
+            }
+            else
+            {
+                particles[i].position = new Vector3C(1000.0f, 1000.0f, 1000.0f);
+                particles[i].acceleration = Vector3C.zero;
+                particles[i].velocity = Vector3C.zero;
+                particles[i].force = Vector3C.zero;
+            }
+            
         }
-        return particles;
+    }
+
+    public void InitCascade()
+    {
+        for (int i = 0; i < particles.Length; ++i)
+        {
+            if (!particles[i].isInitialized)
+            {
+                particles[i] = new Particle(true);
+                particles[i].position = settingsCascade.PointA + (settingsCascade.PointB - settingsCascade.PointA) * RandomFloatBetweenRange(0, 1);
+                particles[i].positionLast = particles[i].position;
+                //particles[i].force += new Vector3C(0.0f, RandomFloatBetweenRange(settingsCascade.minForce, settingsCascade.maxForce), 0.0f);
+                particles[i].force += new Vector3C(RandomFloatBetweenRange(settingsCascade.minForce , settingsCascade.maxForce) * RandomFloatBetweenRange(0, 1), RandomFloatBetweenRange(settingsCascade.minForce, settingsCascade.maxForce) * RandomFloatBetweenRange(0, 1), RandomFloatBetweenRange(settingsCascade.minForce, settingsCascade.maxForce) * RandomFloatBetweenRange(0, 1));
+                particles[i].particleLife = RandomFloatBetweenRange(settingsCascade.minParticleLife, settingsCascade.maxParticleLife);
+                particles[i].isInitialized = true;
+            }
+        }
+    }
+    
+    public float RandomFloatBetweenRange(float min, float max)
+    {
+        return (float)rnd.NextDouble() * (max - min) + min;
     }
 
     public void Debug()
