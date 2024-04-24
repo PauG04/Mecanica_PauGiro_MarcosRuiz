9073e6e812ccdd88c58653c0d8c3bdf1a71fe405
PAUGIROGARCIA <pau.giro@enti.cat>
2024-04-24 20:30:16 +0200

    Matrix

--- Assets/AA2_Delivery/AA2_Rigidbody.cs
+++ Assets/AA2_Delivery/AA2_Rigidbody.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Data;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [System.Serializable]
 public class AA2_Rigidbody
@@ -18,31 +21,113 @@ public class AA2_Rigidbody
     }
     public SettingsCollision settingsCollision;
 
+    [System.Serializable]
     public struct CubeRigidbody
     {
         public Vector3C position;
+        public Vector3C lastPosition;
         public Vector3C euler;
+        public Vector3C force;
         public Vector3C size;
-        public Vector3 linearVelocity;
-        public Vector3 angularVelocity;
+        public Vector3C linearVelocity;
+        public Vector3C angularVelocity;
+        public Vector3C acceleration;
         public float inertialTension;
         public float density;
+        public float mass;
+
+        public Vector3C[] vertexPositions;
 
         public CubeRigidbody(Vector3C _position, Vector3C _size, Vector3C _euler)
         {
             position = _position;
+            lastPosition = Vector3C.zero;
             size = _size;
             euler = _euler;
-            linearVelocity = Vector3.zero;
-            angularVelocity = Vector3.zero;
+            force = Vector3C.zero;
+            linearVelocity = Vector3C.zero;
+            angularVelocity = Vector3C.zero;
+            acceleration = Vector3C.zero;
             inertialTension = 0;
-            density = 0;
+            density = 1f;
+            mass = .1f;
+            vertexPositions = new Vector3C[8];
+        }
+        public void Euler(float dt, Vector3C gravity)
+        {
+            lastPosition = position;
+            acceleration = (force / mass) + gravity;
+            linearVelocity += acceleration * dt;
+            euler += angularVelocity * dt;
+            position += linearVelocity * dt;
+
+            force = Vector3C.zero;
+        }
+
+        public void CalculateVertexPositions()
+        {
+            vertexPositions[0] = new Vector3C(position.x + size.x / 2, position.y + size.y / 2, position.z + size.z / 2);
+            vertexPositions[1] = new Vector3C(position.x - size.x / 2, position.y + size.y / 2, position.z + size.z / 2);
+            vertexPositions[2] = new Vector3C(position.x + size.x / 2, position.y + size.y / 2, position.z - size.z / 2);
+            vertexPositions[3] = new Vector3C(position.x - size.x / 2, position.y + size.y / 2, position.z - size.z / 2);
+            vertexPositions[4] = new Vector3C(position.x + size.x / 2, position.y - size.y / 2, position.z + size.z / 2);
+            vertexPositions[5] = new Vector3C(position.x - size.x / 2, position.y - size.y / 2, position.z + size.z / 2);
+            vertexPositions[6] = new Vector3C(position.x + size.x / 2, position.y - size.y / 2, position.z - size.z / 2);
+            vertexPositions[7] = new Vector3C(position.x - size.x / 2, position.y - size.y / 2, position.z - size.z / 2);
+
+            MatrixC xMatrix = MatrixC.RotateX(euler.x);
+            for(int i = 0; i < vertexPositions.Length; i++)
+            {
+                vertexPositions[i].x = (xMatrix * vertexPositions[i]).x;
+            }
+        }
+
+        public void CalculateCollision(PlaneC[] planes, float bounce)
+        {
+            double distancePlane;
+            CalculateVertexPositions();
+
+            for (int i = 0; i < planes.Length; i++)
+            {
+                for (int j = 0; j < vertexPositions.Length; j++)
+                {
+                    Vector3C vector = vertexPositions[j] - planes[i].position;
+                    distancePlane = Vector3C.Dot(planes[i].normal, vector);
+
+                    if (distancePlane <= 0)
+                    {
+                        CollisionReaction(planes[i], bounce, vertexPositions[j]);
+                    }
+                }
+            }
+        }
+
+        public void CollisionReaction(PlaneC plane, float bounce, Vector3C vertex)
+        {
+            Vector3C lastVertexDirection = (lastPosition - position).normalized;
+            Vector3C lastVertexPosition = vertex + (lastVertexDirection * Math.Abs(Vector3C.Dot(lastPosition, position)));
+
+            Vector3C newVertexPosition = plane.IntersectionWithLine(new LineC(lastVertexPosition, vertex));
+            Vector3C newPosition = position + (lastVertexDirection * Math.Abs(Vector3C.Dot(vertex, newVertexPosition)));
+            position = newPosition;
+
+            float isolatedDotProduct = (linearVelocity * plane.normal) / plane.normal.magnitude;
+
+            Vector3C normalVelocity = plane.normal * isolatedDotProduct;
+            Vector3C tangentVelocity = linearVelocity - normalVelocity;
+            linearVelocity = (-normalVelocity + tangentVelocity) * bounce;
+        }
+
+        public void Update(float dt, Vector3C gravity, PlaneC[] planes, float bounce)
+        {
+            Euler(dt, gravity);
+            CalculateCollision(planes, bounce);
         }
     }
     public CubeRigidbody crb = new CubeRigidbody(Vector3C.zero, new(.1f,.1f,.1f), Vector3C.zero);
     public void Update(float dt)
     {
-
+        crb.Update(dt, settings.gravity, settingsCollision.planes, settings.bounce);
     }
 
     public void Debug()
---
+++ Assets/Common_Delivery/MatrixC.cs
@@ -0,0 +1,130 @@
+using System;
+using System.Runtime.CompilerServices;
+
+[System.Serializable]
+public struct MatrixC
+{
+    #region FIELDS
+    public float[,] data;
+    public int size;
+    #endregion
+
+    #region PROPIERTIES
+    #endregion
+
+    #region CONSTRUCTORS+
+    public MatrixC(float[,] data)
+    {
+        this.data = data;
+        size = (int)Math.Sqrt(data.Length);
+    }
+    #endregion
+
+    #region OPERATORS
+    public static Vector3C operator *(MatrixC m, Vector3C a)
+    {
+        return new Vector3C(m.data[0, 0] * a.x + m.data[0, 1] * a.y + m.data[0, 2] * a.z,
+            m.data[1, 0] * a.x + m.data[1, 1] * a.y + m.data[1, 2] * a.z,
+            m.data[2, 0] * a.x + m.data[2, 1] * a.y + m.data[2, 2] * a.z);
+    }
+
+    public static MatrixC operator *(MatrixC m1, MatrixC m2)
+    {
+        float[,] results = new float[m1.size, m1.size];
+
+        for (int i = 0; i < m1.size; i++)
+        {
+            for (int j = 0; j < m2.size; j++)
+            {
+                for (int k = 0; k < m1.size; k++)
+                {
+                    results[i, j] += m1.data[i, k] * m2.data[k, j];
+                }
+            }
+        }
+
+        return new MatrixC(results);
+    }
+    #endregion
+
+    #region METHODS
+    public void Rotate()
+    {
+
+    }
+
+    public float Determintate3x3(MatrixC m)
+    {
+        if (m.size != 3)
+        {
+            return 0;
+        }
+        float determinate =
+            ((m.data[0, 0] * m.data[1, 1] * m.data[2, 2]) + (m.data[0, 1] * m.data[1, 2] * m.data[2, 0]) + (m.data[0, 2] * m.data[1, 0] * m.data[2, 1])) -
+            ((m.data[0, 2] * m.data[1, 1] * m.data[2, 0]) + (m.data[0, 0] * m.data[1, 2] * m.data[2, 1]) + (m.data[0, 1] * m.data[1, 0] * m.data[2, 2]));
+
+
+        return determinate;
+    }
+
+    public MatrixC Transposed(MatrixC m)
+    {
+        MatrixC transposed = new MatrixC();
+
+        for (int i = 0; i < m.size; i++)
+        {
+            for (int j = 0; j < m.size; j++)
+            {
+                transposed.data[j, i] = m.data[i, j];
+            }
+        }
+
+        return transposed;
+    }
+
+    #endregion
+
+    #region FUNCTIONS
+    public MatrixC Inverse(MatrixC m)
+    {
+        MatrixC inverse = new MatrixC();
+
+        if (Determintate3x3(m) == 0)
+        {
+            return new MatrixC();
+        }
+
+        float invDet = 1.0f / Determintate3x3(m);
+
+        inverse.data[0, 0] = (m.data[1, 1] * m.data[2, 2] - m.data[1, 2] * m.data[2, 1]) * invDet;
+        inverse.data[0, 1] = (m.data[0, 2] * m.data[2, 1] - m.data[0, 1] * m.data[2, 2]) * invDet * -1;
+        inverse.data[0, 2] = (m.data[0, 1] * m.data[1, 2] - m.data[0, 2] * m.data[1, 1]) * invDet;
+        inverse.data[1, 0] = (m.data[1, 2] * m.data[2, 0] - m.data[1, 0] * m.data[2, 2]) * invDet * -1;
+        inverse.data[1, 1] = (m.data[0, 0] * m.data[2, 2] - m.data[0, 2] * m.data[2, 0]) * invDet;
+        inverse.data[1, 2] = (m.data[0, 2] * m.data[1, 0] - m.data[0, 0] * m.data[1, 2]) * invDet * -1;
+        inverse.data[2, 0] = (m.data[1, 0] * m.data[2, 1] - m.data[1, 1] * m.data[2, 0]) * invDet;
+        inverse.data[2, 1] = (m.data[0, 1] * m.data[2, 0] - m.data[0, 0] * m.data[2, 1]) * invDet * -1;
+        inverse.data[2, 2] = (m.data[0, 0] * m.data[1, 1] - m.data[0, 1] * m.data[1, 0]) * invDet;
+
+        return Transposed(inverse);
+    }
+
+    public static MatrixC RotateX(float angle)
+    {
+        MatrixC rotateX = new MatrixC(new float[3,3]);
+
+        rotateX.data[0, 0] = 1;
+        rotateX.data[0, 1] = 0;
+        rotateX.data[0, 2] = 0;
+        rotateX.data[1, 0] = 0;
+        rotateX.data[1, 1] = (float)Math.Cos(angle);
+        rotateX.data[1, 2] = (float)Math.Sin(-angle);
+        rotateX.data[2, 0] = 0;
+        rotateX.data[2, 1] = (float)Math.Sin(angle);
+        rotateX.data[2, 2] = (float)Math.Cos(angle);
+
+        return rotateX;
+    }
+    #endregion
+
+}
